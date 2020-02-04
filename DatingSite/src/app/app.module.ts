9958f36091d02b41434e3d06931956c4af5d8c57
TiborB <tiborbarczi@gmail.com>
2020-02-04 13:48:26 +0100

    general.css update, + contact component

--- DatingSite/src/app/app.module.ts
+++ DatingSite/src/app/app.module.ts
@@ -7,7 +7,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './components/header-component/header-component.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { TesterComponentComponent } from './components/tester-component/tester-component.component';
-import { RegistrationComponent } from './components/registration/registration.component';
+import { SigninComponentComponent } from './components/signin-component/signin-component.component';
+import { FooterComponentComponent } from './components/footer-component/footer-component.component';
+import { ContactComponentComponent } from './components/contact-component/contact-component.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { RegistrationComponent } from './components/registration/registration.co
     HeaderComponentComponent,
     MainPageComponent,
     TesterComponentComponent,
-    RegistrationComponent
+    SigninComponentComponent,
+    FooterComponentComponent,
+    ContactComponentComponent
   ],
   imports: [
     BrowserModule,
