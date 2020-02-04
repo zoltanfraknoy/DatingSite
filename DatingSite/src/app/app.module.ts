580e57cd984435b75de13352b637480f831f4191
Zoltán <zoltanfraknoy@gmail.com>
2020-02-04 13:35:16 +0100

    Megamenu css

--- DatingSite/src/app/app.module.ts
+++ DatingSite/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './components/header-component/header-component.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { TesterComponentComponent } from './components/tester-component/tester-component.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponentComponent,
     MainPageComponent,
-    TesterComponentComponent
+    TesterComponentComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
