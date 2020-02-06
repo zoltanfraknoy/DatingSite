64143f1ce9d70c525f766f93236039c1261ee265
Zoltán <zoltanfraknoy@gmail.com>
2020-02-06 13:58:12 +0100

    fixed registration routing

--- DatingSite/src/app/app-routing.module.ts
+++ DatingSite/src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 import { from } from 'rxjs';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { LoginComponentComponent } from './components/login-component/login-component.component';
-import {TesterComponentComponent} from './components/tester-component/tester-component.component';
+import { TesterComponentComponent } from './components/tester-component/tester-component.component';
 import { MyProfileComponentComponent } from './components/my-profile-component/my-profile-component.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'main' },
   { path: 'main', component: MainPageComponent },
-  { path: 'registration', component: MainPageComponent },
+  { path: 'registration', component: RegistrationComponent },
   { path: 'login', component: LoginComponentComponent },
   //for test!!!!
   { path: 'test', component: TesterComponentComponent },
