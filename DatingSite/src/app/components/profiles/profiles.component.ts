0a0a78c892954def015939ec5b72cd1daf648199
Zoltán <zoltanfraknoy@gmail.com>
2020-02-10 23:11:27 +0100

    Get users, get user, updateUsers, Profiles subscribe, headerBug realized

--- DatingSite/src/app/components/profiles/profiles.component.ts
+++ DatingSite/src/app/components/profiles/profiles.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
+import { Subscription } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profiles',
   templateUrl: './profiles.component.html',
   styleUrls: ['./profiles.component.scss']
 })
-export class ProfilesComponent implements OnInit {
+export class ProfilesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  users: User[];
+  usersSubscription: Subscription;
 
+  constructor(private userService: UserService) {
+    this.users=[];
+
+   }
+//Feliratkozunk a service getUsers metódusára és megkapunk egy csini users tömböt
   ngOnInit() {
+    this.usersSubscription = this.userService.getUsers().subscribe(
+      users => {
+        this.users = users;
+      }
+    );
+
+  }
+
+  ngOnDestroy() {
+    this.usersSubscription.unsubscribe();
   }
 
 }
--- DatingSite/src/app/services/user.service.ts
+++ DatingSite/src/app/services/user.service.ts
@@ -11,24 +11,43 @@ import { share, map } from 'rxjs/operators';
 export class UserService {
 
   private users: BehaviorSubject<User[]>;
-  private readonly SERVER_URL = 'http://localhost:8080/rest/register';
+  private readonly SERVER_URL = 'http://localhost:8080/rest/';
 
   constructor(private http: HttpClient) {
     this.users = new BehaviorSubject([]);
   }
 
+ 
+
   addUser(usr: User): void {
     const json = JSON.stringify(usr);
     // írd át promiszra baszki
     const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
-    this.http.post<UserResponse>(this.SERVER_URL, json, {withCredentials: true, headers}).subscribe();
+    this.http.post<UserResponse>(this.SERVER_URL+"register", json, {withCredentials: true, headers}).subscribe();
   }
 
   loginUser(usr: UserLogin) {
     console.log('login try');
     // TODO
   }
+//Lekérjük a users tömböt
+  getUsers(): BehaviorSubject<User[]> {
+    this.http.get<UserResponse>(
+      this.SERVER_URL,
+      //szűrés hogyan??? default? maximalizálni a kapott válaszokat?
+       { withCredentials: true })
+      .subscribe(resp => this.updateUsers(resp));
+    return this.users;
+  }
 
+  //Ha a response sikeres frissítjük a változást
+  private updateUsers(response: UserResponse) {
+    if (response.success) {
+      this.users.next(response.users);
+    }
+  }
+
+  //Lekérünk 1 usert az ID alapján
   public getUser(id: number): Observable<User> {
     return this.http.get<UserResponse>(
       this.SERVER_URL + '?id=' + id,
