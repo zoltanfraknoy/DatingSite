97af36778b29bacc31b562166969bbbe2491b68d
Zoltán <zoltanfraknoy@gmail.com>
2020-02-14 11:56:13 +0100

    nyehh

--- DatingSite/src/app/components/user-filter/user-filter.component.ts
+++ DatingSite/src/app/components/user-filter/user-filter.component.ts
@@ -22,13 +22,14 @@ export class UserFilterComponent implements OnInit {
     this.form = new FormGroup(
       {
 
-        ownGender: new FormControl('Man'),
+        ownGender: new FormControl('Man',),
         targetGender: new FormControl('Woman'),
         ageMin: new FormControl(18),
-        ageMax: new FormControl(36)
+        ageMax: new FormControl(36),
+        pageNumber: new FormControl(1)
       }
     );
-      this.search = new EventEmitter();
+    this.search = new EventEmitter();
 
   }
 
@@ -36,9 +37,11 @@ export class UserFilterComponent implements OnInit {
     this.userService.getMyProfile().subscribe(u => {
       this.user = u;
       this.form.controls['ageMin'].setValue(this.user.minAge);
+      console.log(this.form.controls['ageMin']);
       this.form.controls['ageMax'].setValue(this.user.maxAge);
       this.form.controls['ownGender'].setValue(this.user.gender);
       this.form.controls['targetGender'].setValue(this.user.interest);
+      this.form.controls['pageNumber'].setValue(1);
     });
     this.createForm();
   }
